e1803a61315cb8e53b8c951eab9eca6d1ef68e41
Jim Bennett <jimbobbennett@mac.com>
2015-04-25 23:18:08 +0100

    updated build scripts

--- RestAndRelaxForPlex.nuspec/BuildNugetPackage.ps1
+++ RestAndRelaxForPlex.nuspec/BuildNugetPackage.ps1
@@ -1,3 +1,21 @@
-del *.nupkg; 
-nuget pack .\RestAndRelaxForPlex.nuspec\JimBobBennett.RestAndRelaxForPlex.nuspec -Symbols -Properties Configuration=Release;
-cp *.nupkg c:\NuGet
+Param
+(
+    [Parameter(Mandatory=$True)][string]$version,
+    [bool]$push = $false,
+    [string]$copyTo
+) 
+
+& 'C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe' .\RestAndRelaxForPlex.sln /property:Configuration=Release /Target:Rebuild
+& 'nuget.exe' pack .\RestAndRelaxForPlex.nuspec\JimBobBennett.RestAndRelaxForPlex.nuspec -Properties Configuration=Release -Version $version -Symbols
+
+if ($copyTo)
+{
+    cp .\JimBobBennett.RestAndRelaxForPlex.$version.nupkg $copyTo
+    cp .\JimBobBennett.RestAndRelaxForPlex.$version.symbols.nupkg $copyTo
+}
+
+
+if ($push)
+{
+    nuget.exe push .\JimBobBennett.RestAndRelaxForPlex.$version.nupkg
+}
